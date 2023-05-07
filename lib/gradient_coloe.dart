c150c262c56cf2efc63de6054b1fafbc53655dcf
arafatmollah <arafatshuvo18@gmail.com>
2023-05-07 10:45:45 +0600

    flutter leaning

--- lib/gradient_coloe.dart
+++ lib/gradient_coloe.dart
@@ -1,19 +1,21 @@
 import 'package:dice_roll/style_text.dart';
 import 'package:flutter/material.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 
 class GradientColor extends StatelessWidget{
   const GradientColor({super.key});
   @override
   Widget build(context){
     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
+          decoration:  BoxDecoration(
+            gradient:  LinearGradient(
               colors: 
-              [Color.fromRGBO(26, 1, 252, 0.722),
+              const [Color.fromRGBO(26, 1, 252, 0.722),
               Color.fromARGB(135, 13, 0, 190)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+              begin: startAlignment,
+              end: endAlignment,
 
             ),
           ),
